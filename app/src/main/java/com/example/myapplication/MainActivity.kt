ca8ac2d50b9771e5844efe7f305856aab697ad70
Papan Das <167051110+daspapan@users.noreply.github.com>
2024-05-28 23:58:40 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Surface
+import com.google.android.material.theme.overlay.MaterialThemeOverlay
+import org.w3c.dom.Text
+import java.nio.file.WatchEvent.Modifier
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        setContent {
+            MyFirstAppTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background){
+                    Greeting("Papan")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier){
+    Text(
+        text = "Welcome $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    MyFirstAppTheme {
+        Greeting("Papan Das")
     }
 }
